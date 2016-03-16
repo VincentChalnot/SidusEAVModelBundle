b24e2054f93afb454472250bf124d1ecd4467c62
Vincent Chalnot <vchalnot@clever-age.com>
2016-03-16 13:26:39 +0100

    Changed family selector to use family codes as option's values instead of randomly generated ids

--- Form/Type/FamilySelectorType.php
+++ Form/Type/FamilySelectorType.php
@@ -4,7 +4,13 @@ namespace Sidus\EAVModelBundle\Form\Type;
 
 
 use Sidus\EAVModelBundle\Configuration\FamilyConfigurationHandler;
+use Sidus\EAVModelBundle\Exception\MissingFamilyException;
+use Sidus\EAVModelBundle\Model\FamilyInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Exception\AccessException;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -21,20 +27,79 @@ class FamilySelectorType extends AbstractType
         $this->familyConfigurationHandler = $familyConfigurationHandler;
     }
 
+    /**
+     * @inheritDoc
+     * @throws \InvalidArgumentException
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new CallbackTransformer(
+            function ($originalData) use ($options) {
+                if ($originalData instanceof FamilyInterface) {
+                    $originalData = $originalData->getCode();
+                }
+                return $originalData;
+            },
+            function ($submittedData) {
+                if ($submittedData instanceof FamilyInterface) {
+                    // Should actually never happen ?
+                    return $submittedData;
+                }
+                try {
+                    return $this->familyConfigurationHandler->getFamily($submittedData);
+                } catch (MissingFamilyException $e) {
+                    throw new \InvalidArgumentException($e->getMessage(), 0, $e);
+                }
+            }
+        ));
+    }
 
+    /**
+     * @inheritdoc
+     * @throws \Exception
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $choices = [];
-        foreach ($this->familyConfigurationHandler->getFamilies() as $family) {
-            if ($family->isInstantiable()) {
-                $choices[ucfirst($family)] = $family;
-            }
-        }
-
         $resolver->setDefaults([
             'choices_as_values' => true,
-            'choices' => $choices,
+            'choices' => null,
+            'families' => null,
         ]);
+
+        $resolver->setNormalizer('families', function (Options $options, $values) {
+            if (null === $values) {
+                $values = $this->familyConfigurationHandler->getFamilies();
+            }
+            $families = [];
+            foreach ($values as $value) {
+                if (!$value instanceof FamilyInterface) {
+                    $value = $this->familyConfigurationHandler->getFamily($value);
+                }
+                if ($value->isInstantiable()) {
+                    $families[$value->getCode()] = $value;
+                }
+            }
+            return $families;
+        });
+        $resolver->setNormalizer('choices_as_values', function (Options $options, $value) {
+            if ($value !== true) {
+                throw new \UnexpectedValueException("'choices_as_values' must be true (and is by default)");
+            }
+            return true;
+        });
+        $resolver->setNormalizer('choices', function (Options $options, $value) {
+            if (null !== $value) {
+                throw new \UnexpectedValueException("'choices' options is not supported for family selector, please use 'families' option");
+            }
+            $choices = [];
+            /** @var FamilyInterface $family */
+            foreach ($options['families'] as $family) {
+                if ($family->isInstantiable()) {
+                    $choices[ucfirst($family)] = $family->getCode();
+                }
+            }
+            return $choices;
+        });
     }
 
     public function getParent()
